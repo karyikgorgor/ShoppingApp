53927bbf0eb0bfc0044746179bd8fc9c051dc904
karyikgorgor <mryik34@gmail.com>
2018-01-15 16:47:14 +0800

    Testing commitment from Android Studio

---
+++ app/src/main/java/model/CustomerAccount.java
@@ -0,0 +1,16 @@
+package model;
+
+/**
+ * Created by IMCKY on 15/1/2018.
+ */
+
+public class CustomerAccount {
+
+    private String a;
+    private String b;
+
+    public CustomerAccount (String a, String b) {
+        this.a = a;
+        this.b = b;
+    }
+}
