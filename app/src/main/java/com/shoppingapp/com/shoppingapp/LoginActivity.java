14a4185bc37a50a8ce1d05680d61fd1c8364b494
karyikgorgor <mryik34@gmail.com>
2018-01-15 18:00:27 +0800

    Second commitment test

--- app/src/main/java/com/shoppingapp/com/shoppingapp/LoginActivity.java
+++ app/src/main/java/com/shoppingapp/com/shoppingapp/LoginActivity.java
@@ -2,6 +2,7 @@ package com.shoppingapp.com.shoppingapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    private void login () {
+        Toast.makeText(this, "lol", Toast.LENGTH_SHORT).show();
     }
 }
